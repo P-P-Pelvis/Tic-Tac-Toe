5ca6cdc9ecda4978e800a9dd235057964f289f9c
Edwin Tovar <poohwinnie2800@gmail.com>
2025-11-05 08:58:29 -0800

    Initial Commit

---
+++ Tic Tac Toe/Tic_Tac_ToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_ToeApp.swift
+//  Tic Tac Toe
+//
+//  Created by Edwin Tovar on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_ToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
